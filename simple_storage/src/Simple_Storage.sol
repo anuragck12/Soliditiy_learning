42be394d02367154a62666f5566606eac07e2949
anuragck12 <anuragck22@gmail.com>
2024-08-26 10:27:06 +0530

    changed

--- simple_storage/src/Simple_Storage.sol
+++ simple_storage/src/Simple_Storage.sol
@@ -1,16 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 contract SimpleStorage{
-    // struct person{
-    //     string namePerson;
-    //     uint256 favouriteNumber;
-    // }
-    mapping (string => uint) public nameToFav;
+    uint256 public favouriteNo;
+    struct person{
+        string name;
+        uint256 favNo;
+    }
+    person[] public listOfPersons;
+    mapping (string => uint256) public nameToFav;
 
     function favNo(uint256 _favNo,string calldata _name) public{
+       listOfPersons.push(person(_name,_favNo));
        nameToFav[_name]=_favNo;
+       favouriteNo=_favNo;
     }
     function viewFavNo(string calldata _name) public view returns(uint256){
         return nameToFav[_name];
+    }
+    function retrive() public view returns(uint256){
+        return favouriteNo;
     }
 }
