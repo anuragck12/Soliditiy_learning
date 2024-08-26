cd00583814fd136d5e563d5a078e7b13c4edf477
anuragck12 <anuragck22@gmail.com>
2024-08-26 10:04:00 +0530

    added mapping

--- simple_storage/src/Simple_Storage.sol
+++ simple_storage/src/Simple_Storage.sol
@@ -1,11 +1,16 @@
-//SPDX-License:MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 contract SimpleStorage{
-    uint256 favouriteNumber;
-    function favNo(uint256 _favNo) public{
-        favouriteNumber=_favNo;
+    // struct person{
+    //     string namePerson;
+    //     uint256 favouriteNumber;
+    // }
+    mapping (string => uint) nameToFav;
+
+    function favNo(uint256 _favNo,string calldata _name) public{
+       nameToFav[_name]=_favNo;
     }
-    function viewFavNo() public view returns(uint256){
-        return favouriteNumber;
+    function viewFavNo(string calldata _name) public view returns(uint256){
+        return nameToFav[_name];
     }
 }
