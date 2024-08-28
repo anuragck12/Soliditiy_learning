304e2fba76953ad6f06197a771eeb00707834062
anuragck12 <anuragck22@gmail.com>
2024-08-28 18:20:13 +0530

    edited readme about deploying contract

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Soliditiy_learning
 1. simple storage- just storing a number on the blockchain and retriving that number => public functions
+
+2.for deploying the contract we need to run
+first of all we need to create an local network which can be ceated from anvil
+first command:
+anvil
+then create an another bash ,then type this command
+forge create "contract name" --rpc-url <url> --interactive
+--interactive for typing privatekey on compilation
--- simple_storage/src/Simple_Storage.sol
+++ simple_storage/src/Simple_Storage.sol
@@ -1,27 +1,31 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-contract SimpleStorage{
+
+contract SimpleStorage {
     uint256 public favouriteNo;
-    struct person{
+    struct person {
         string name;
         uint256 favNo;
     }
     person[] public listOfPersons;
-    mapping (string => uint256) public nameToFav;
+    mapping(string => uint256) public nameToFav;
 
-    function favNo(uint256 _favNo,string calldata _name) public{
-       listOfPersons.push(person(_name,_favNo));
-       nameToFav[_name]=_favNo;
-       favouriteNo=_favNo;
+    function favNo(uint256 _favNo, string calldata _name) public {
+        listOfPersons.push(person(_name, _favNo));
+        nameToFav[_name] = _favNo;
+        favouriteNo = _favNo;
     }
-    function viewFavNo(string calldata _name) public view returns(uint256){
+
+    function viewFavNo(string calldata _name) public view returns (uint256) {
         return nameToFav[_name];
     }
-    function retrive() public view returns(uint256){
+
+    function retrive() public view returns (uint256) {
         return favouriteNo;
     }
- //for using pure we can't read the variables inside the contract and ofcourse we can't modify them eg
-function pureFn() public pure returns (uint256){
-   return 7;
-}
+
+    //for using pure we can't read the variables inside the contract and ofcourse we can't modify them eg
+    function pureFn() public pure returns (uint256) {
+        return 7;
+    }
 }
